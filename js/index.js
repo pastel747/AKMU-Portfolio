d104c7b68ef80c1ef417572ace7cb5cfa011f9a5
yeon <68599757+pastel747@users.noreply.github.com>
2020-11-16 21:16:13 +0900

    js 정리

--- js/index.js
+++ js/index.js
@@ -1,21 +1,17 @@
 $(document).ready(function(){
-    swiperTEST4();
-    test();
-    test2();
-    // test3();
-    test4();
-    // test5();
-    // test6();
+    swiperSetting();
+    playAreaSwiperSelect();
+    playAreaAlbumListActive();
+    playAreaAlbumImgChange();
     test7();
-    // videoSk();
     playTrack();
     timeGet();
-    vid();
     test8();
+    test9();
     audioTotal();
 });
 
-function swiperTEST4(){
+function swiperSetting(){
     var swiper = new Swiper('.swiper-container', {
     slidesPerView: 5,
     spaceBetween: 20,
@@ -39,7 +35,7 @@ function swiperTEST4(){
     }
 }
 
-function test(){
+function playAreaSwiperSelect(){
     $('.swiper-wrapper div').click(function(){ //div 클릭시
         $('.playArea>ul.playlist>li').removeClass('albumActive'); //연동되는 li들에 일괄적으로 클래스를 빼준다
         $('.swiper-wrapper>div').removeClass('albumActive'); // li들처럼 div도 클래스를 빼준다
@@ -49,10 +45,10 @@ function test(){
     });
 }
 
-function test2(){  
-    var test = $('section.playArea>ul.playlist>li ol li');
-    test.click(function(){ 
-        test.removeClass('listActive'); 
+function playAreaAlbumListActive(){  
+    var albumList = $('.playArea>ul.playlist>li ol li');
+    albumList.click(function(){ 
+        albumList.removeClass('listActive'); 
         $(this).addClass('listActive');
     });
 }
@@ -66,14 +62,14 @@ function test2(){
 //     });
 // }
 
-function test3(){
-    var test = $('section.playArea>img');
-    var test2 = $('.swiper-wrapper div.albumActive img').attr('src');
+// function test3(){
+//     var test = $('section.playArea>img');
+//     var test2 = $('.swiper-wrapper div.albumActive img').attr('src');
 
-    $('.swiper-wrapper div').click(function(){
-        test.attr('src', test2);
-    });
-}
+//     $('.swiper-wrapper div').click(function(){
+//         test.attr('src', test2);
+//     });
+// }
 
 // function test4(){
 //     var test = $('section.playArea>img');
@@ -84,12 +80,12 @@ function test3(){
 //     });
 // }
 
-function test4(){
-    var test = $('section.playArea>img');
+function playAreaAlbumImgChange(){
+    var albumImg = $('.playArea>img');
 
     $('.swiper-wrapper div').click(function(){
-        var test2 = $(this).find('img').attr('src');
-        test.attr('src', test2);
+        var swiperImgAttr = $(this).find('img').attr('src');
+        albumImg.attr('src', swiperImgAttr);
     });
 }
 
@@ -106,9 +102,8 @@ function test4(){
 //     });
 // }
 
-
 function test7(){
-    var albumList = $('section.playArea>ul.playlist li>ol>li');
+    var albumList = $('.playArea>ul.playlist li ol li');
     
     albumList.click(function(){
         document.getElementById("playName").removeAttribute("id"); 
@@ -156,6 +151,7 @@ function timeGet(){
         document.getElementById('listTime').innerHTML=getTime;
     });
 }
+
 function test8(){
     var playButtonSelect = $('section.playArea>div:last-child>input#playAreabutton');
     var statusButton = playButtonSelect.css('background-image');
@@ -172,39 +168,43 @@ function test8(){
     });
 }
 
-function vid(){
-    var video = $('section.videoArea video');
 
-    function play(){
-        if(video.pause){
-            videoBG.pause();
-        }else{
-            videoBG.play();
-        }
-    }
+
+
+function test9(){
+    var albumPlay =  new Array( '../audio/file_example.mp3', '../audio/file_example.mp3', '../audio/file_example.mp3' );
 }
-
-
-
-var video = $('section.videoArea>div>video');
-var videoBG = $('section.videoArea>video');
-function play(){
-    if(video.pause){
-        videoBG.pause();
-    }else{
-        videoBG.play();
-    }
-}
-
 
 function audioTotal(){
     var audioList = $('section.playArea>ul.playlist>li>ol>li');
 
+    var albumArray = new Array(Full_Album1, Single_Album1, Single_Album2, Single_Album3, Full_Album2, Single_Album4, Single_Album5, Full_Album3);
+    var Full_Album3 = new Array('../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3');
+    var Full_Album2 = new Array('../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3');
+    var Full_Album1 = new Array('../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3', '../audio/file_example2.mp3','../audio/file_example.mp3');
+
+    var Single_Album5 = new Array('../audio/file_example.mp3', '../audio/file_example2.mp3');
+    var Single_Album4 = new Array('../audio/file_example2.mp3', '../audio/file_example.mp3');
+    var Single_Album3 = new Array('../audio/file_example.mp3');
+    var Single_Album2 = new Array('../audio/file_example2.mp3', '../audio/file_example.mp3','../audio/file_example2.mp3', '../audio/file_example.mp3','../audio/file_example2.mp3', '../audio/file_example.mp3');
+    var Single_Album1 = new Array('../audio/file_example.mp3');
+
+
+ 
     audioList.click(function(){
-        var audio = new Audio("../audio/file_example.mp3");
+        
+        var albumIndex = $('section.playArea>ul.playlist>li.albumActive').index();
+        alert(albumIndex);
+        var listInAlbumIndex = $('section.playArea>ul.playlist>li.albumActive>ol>li.listActive').index();
+        alert(listInAlbumIndex);
+        
+        // var audio = new Audio("../audio/file_example.mp3");
+        var audio = new Audio("../audio/file_example.mp3");  //albumArray[albumIndex] 와 그 안에 audio file url 가져오기 find 쓰나?
+
+
         audio.loop = false;
         audio.volume = 0.3;
         audio.play();
-        
+
     })
 }
